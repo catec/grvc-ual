30ee2f39a332a36a4e392b91e420684033cf9a62
Pedro <pedsancue@gmail.com>
2022-05-06 11:48:56 +0200

    Revert "Pseudo-minimum version of ual_backend_mavros and docker containerization"
    This reverts commit d3fd223df5d9cd08f9a2822952229882bd22d50f.

--- ros_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros environment
-source "/opt/ros/melodic/setup.bash"
-source "/catkin_ws/devel/setup.bash"
-
-roslaunch ual_backend_mavros docker_ual_sim_server.launch
