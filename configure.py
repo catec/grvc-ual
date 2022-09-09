e6238a9a8c983fed188cedfa81797c4b8740d7b2
Marco A. Montes Grova <marrcogrova@gmail.com>
2022-09-09 11:25:23 +0200

    Modified configure file to be able to use in any ros version

--- configure.py
+++ configure.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 
-class bcolors:    
+class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -13,21 +13,24 @@ class bcolors:
 
 def main():
 
-    print bcolors.OKBLUE + bcolors.BOLD + """   ____ ______     ______    _   _   _    _     
+    ros_version_proc = subprocess.Popen(["rosversion", "-d"], stdout=subprocess.PIPE)
+    ros_version = ros_version_proc.communicate()[0].decode("utf-8").strip()
+
+    print (bcolors.OKBLUE + bcolors.BOLD + """   ____ ______     ______    _   _   _    _     
   / ___|  _ \\ \\   / / ___|  | | | | / \\  | |    
  | |  _| |_) \\ \\ / / |      | | | |/ _ \\ | |    
  | |_| |  _ < \\ V /| |___   | |_| / ___ \\| |___ 
   \\____|_| \\_\\ \\_/  \\____|   \\___/_/   \\_\\_____|                                      
-    """ + bcolors.ENDC
-    print bcolors.BOLD + "> ual_backend_mavros installated in ros melodic docker image:" + bcolors.ENDC
-    
+    """ + bcolors.ENDC)
+    print (bcolors.BOLD + "> ual_backend_mavros installated in ros " + ros_version + " docker image:" + bcolors.ENDC)
+
     subprocess.call("sudo apt-get update", shell=True)
     subprocess.call("sudo apt-get install -y libeigen3-dev", shell=True)
-    subprocess.call("sudo apt-get install -y ros-melodic-joy", shell=True)
-    subprocess.call("sudo apt-get install -y ros-melodic-geodesy", shell=True)
-    subprocess.call("sudo apt-get install -y ros-melodic-mavros", shell=True)
-    subprocess.call("sudo apt-get install -y ros-melodic-mavros-extras", shell=True)
-    subprocess.call("sudo apt-get install -y ros-melodic-tf2-geometry-msgs", shell=True)
+    subprocess.call("sudo apt-get install -y ros-" + ros_version + "-joy", shell=True)
+    subprocess.call("sudo apt-get install -y ros-" + ros_version + "-geodesy", shell=True)
+    subprocess.call("sudo apt-get install -y ros-" + ros_version + "-mavros", shell=True)
+    subprocess.call("sudo apt-get install -y ros-" + ros_version + "-mavros-extras", shell=True)
+    subprocess.call("sudo apt-get install -y ros-" + ros_version + "-tf2-geometry-msgs", shell=True)
     subprocess.call("sudo geographiclib-get-geoids egm96-5", shell=True)
     subprocess.call("sudo usermod -a -G dialout $USER", shell=True)
     subprocess.call("sudo apt-get remove modemmanager", shell=True)
