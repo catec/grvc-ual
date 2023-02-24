517b6abfad9b5ff0e1cb39443ea1550016bc3398
David Tejero Ruiz <dtejero@catec.aero>
2023-02-24 15:00:39 +0100

    Updates to specify global folder path and automatically save with a timestamp

--- uav_abstraction_layer/scripts/save_waypoints.py
+++ uav_abstraction_layer/scripts/save_waypoints.py
@@ -16,6 +16,11 @@ def save_waypoints():
     parser = argparse.ArgumentParser(description='Save waypoints to a yaml file')
     parser.add_argument('-plan_package', type=str, default='uav_abstraction_layer',
                         help='Name of the package where generated plan will be stored')
+    parser.add_argument('-plan_folder', type=str, default='',
+                        help='Name of the folder inside plan_package where generated plan will be stored')
+    parser.add_argument('-plan_file_prefix', type=str, default='recorded_wp',
+                        help='Prefix for the generated plan file')
+    
     args, unknown = parser.parse_known_args()
     # utils.check_unknown_args(unknown)
 
@@ -25,25 +30,28 @@ def save_waypoints():
     pose_url = 'ual/pose'
     rospy.Subscriber(pose_url, PoseStamped, pose_callback)
 
-    # Saved waypoints file name
-    file_name = input("Enter saved waypoints file name (leave empty to use default): ")
-    if file_name == '':
-        file_name = 'wp_default.yaml'
+    # Wait for pose topic
+    rospy.loginfo('Waiting for pose topic...')
+    rospy.wait_for_message(pose_url, PoseStamped)
+    rospy.loginfo('Pose topic found!')
 
-    # Autocomplete file extension
-    if not file_name.endswith('.yaml'):
-        file_name = file_name + '.yaml'
 
-    # Check we are actually receiving pose
-    rospy.loginfo('Reading pose from: %s', pose_url)
-    if current_pose.header.frame_id == '':
-        rospy.logerr('Unable to read pose, assure it is being published!')
-        return
+    # Get plans directory
+    if args.plan_folder == '':
+        plans_dir = rospkg.RosPack().get_path(args.plan_package) + '/plans/'
+    else:
+        if not args.plan_folder.endswith('/'):
+            args.plan_folder = args.plan_folder + '/'
 
-    # Assure that plans folder exists in plan_package
-    plans_dir = rospkg.RosPack().get_path(args.plan_package) + '/plans/'
+        if not args.plan_folder.startswith('/'):
+            plans_dir = rospkg.RosPack().get_path(args.plan_package) + '/' + args.plan_folder 
+        else:
+            plans_dir = args.plan_folder
     subprocess.call("mkdir -p " + plans_dir, shell=True)
-
+        
+    # Get file name with timestamp
+    file_name = args.plan_file_prefix + '_' + rospy.get_time().__str__().split('.')[0] + '.yaml'
+    
     # Open yaml file and save frame_id
     frame_id = current_pose.header.frame_id
     rospy.loginfo('All waypoints in frame_id: %s', frame_id)
@@ -52,9 +60,15 @@ def save_waypoints():
     yaml_file.write('frame_id: ' + frame_id + '\n')
     rospy.loginfo('Waypoints will be saved to: %s', file_url)
 
+    # Check we are actually receiving pose
+    rospy.loginfo('Reading pose from: %s', pose_url)
+    if current_pose.header.frame_id == '':
+        rospy.logerr('Unable to read pose, assure it is being published!')
+        return
+    
     for wp_id in range(1000):
-        if input("Press Enter to save current pose or q to quit... ") == 'q':
-            break
+        if input("Press Enter to save current pose or q to quit and save curent ... ") == 'q':
+            break        
 
         # print(current_pose) # debug!
         wp_to_save = 'wp_' + str(wp_id) + ': [' + \
@@ -78,7 +92,6 @@ def save_waypoints():
         rospy.loginfo('All waypoints saved to: %s', file_url)
     yaml_file.close()
     return
-
 
 if __name__ == "__main__":
     save_waypoints()
